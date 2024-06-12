7516583dcfd2796cf24bd2c52d6a1aff5ac120bc
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-13 00:01:10 +0530

    Rules For var name

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("First code was written in Browser.");
+
+fullname = "Ganesh Suresh Pise";
+console.log(fullname);
+
+console.log("Above example shows about the var in Js. variable means the object values was changed . Eg:Flipcart product.");
+
+comsole.log("Variable: var are cotainer which can store any type of data.");
+console.log("Rules for Variable name : 1> Var name are case sensitive: 'a'and'A' both are different.");
+console.log("                          2>Only letters,Digits,UnderScore(_),and $ allowed (not even space allowed).");
+console.log("                          3>Only letter Underscore(_) and $ allowed they should be a first character.");
+console.log("                          4>Reserved words can not be variable names.()")

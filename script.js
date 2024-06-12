de3fb2c392cb4e26bf3c222f8a6e1c3cbad55e68
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-13 00:05:38 +0530

    -

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ console.log(fullname);
 
 console.log("Above example shows about the var in Js. variable means the object values was changed . Eg:Flipcart product.");
 
-comsole.log("Variable: var are cotainer which can store any type of data.");
-console.log("Rules for Variable name : 1> Var name are case sensitive: 'a'and'A' both are different.");
-console.log("                          2>Only letters,Digits,UnderScore(_),and $ allowed (not even space allowed).");
-console.log("                          3>Only letter Underscore(_) and $ allowed they should be a first character.");
-console.log("                          4>Reserved words can not be variable names.()")
+console.log("Variable: var are cotainer which can store any type of data.");
+console.log("Rules for Variable name :   1> Var name are case sensitive: 'a'and'A' both are different.");
+console.log("                            2> Only letters,Digits,UnderScore(_),and $ allowed (not even space allowed).");
+console.log("                            3> Only letter Underscore(_) and $ allowed they should be a first character.");
+console.log("                            4> Reserved words can not be variable names.()")

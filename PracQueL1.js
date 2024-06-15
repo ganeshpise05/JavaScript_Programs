d9bfcbd372ada041039d1e656133c6c32bd10afa
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-16 00:31:42 +0530

    Lecture:1 Practice questions

---
+++ PracQueL1.js
@@ -0,0 +1,23 @@
+//Q.1:Create a const object called "Product" to store information shown in the picture[name,rating,price,offer]
+
+const Product ={
+    title:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+
+console.log(Product);
+
+//Q.2:Create a const object called "Instagram Profile" to store information.
+
+const Profile ={
+    username:"call_me_funtya",
+    followers:567,
+    following:600,
+    isfollow:false,
+    posts:15,
+    Bio:"Be Remembered for Good !!!"
+};
+
+console.log(Profile)

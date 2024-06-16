17f459e08d98046689cf69daa71bd020f9bb7b6c
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-17 00:10:12 +0530

    Arithmatic Operators

--- Operators.js
+++ Operators.js
@@ -7,3 +7,42 @@ console.log("This is single line comment");
 */
 
 console.log("Multi-line comment");
+
+//Arithmatic Operators
+let a = 10;
+let b = 5;
+
+console.log("Addition of a+b = ",a+b);
+console.log("Substraction of a-b = ",a-b);
+console.log("Multiplication of a*b = ",a*b);
+console.log("Division of a/b = ",a/b);
+console.log("Addition of a+b = ",a+b);
+console.log("Modulus of a%b = ",a%b);
+console.log("Exponential of a**b = ",a**b);//5'2=25
+console.log('===============================')
+
+//Unary Operators 
+let x = 4;
+let y = 9;
+
+//Increment(++)
+console.log('x='+x+' and y='+y);
+console.log('x++ = ',x++);
+console.log('y++ = ',y++);
+
+console.log('++x = ',++x);
+console.log(x);
+
+console.log('++y = ',++y);
+console.log(y);
+console.log('----------------------');
+
+//Decrement(--)
+console.log('x-- = ',x--);
+console.log('y-- = ',y--);
+
+console.log('--x = ',--x);
+console.log(x);
+
+console.log('--y = ',--y);
+console.log(y);

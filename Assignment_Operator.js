c0f0bbf212cc0d32737df07ce868cb27fe547a66
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-17 00:17:09 +0530

    Assignment Operators

---
+++ Assignment_Operator.js
@@ -0,0 +1,12 @@
+//Assignment Operators (=,+=,-=,/=,*=,%=,**=)
+
+let a = 10;
+let b = 5;
+
+console.log('a+=b: ',a+=b);
+console.log('a-=b: ',a-=b);
+console.log('a/=b: ',a/=b);
+console.log('a*=b: ',a*=b);
+console.log('a%=b: ',a%=b);
+console.log('a**=b: ',a**=b);
+ 

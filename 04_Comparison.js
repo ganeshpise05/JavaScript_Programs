c15642a01da1952ff7ad9a0821b896f5b62ff940
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-20 00:37:12 +0530

    Comparison Operator

---
+++ 04_Comparison.js
@@ -0,0 +1,24 @@
+//Comparison Operators
+
+// console.log(2>3);
+// console.log(2>=3);
+// console.log(2!=3);
+// console.log(2==3);
+
+//When we have to compare two attributes then both datatypes must be same
+
+// console.log('2'>3);
+// console.log('02'>3);
+
+//null
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//undefined
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+//===
+console.log("2" === 2);

9d001b2a071082aca45b0b2f18314fd9553416fc
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-20 00:50:57 +0530

    Datatypes

---
+++ 05_Datatypes.js
@@ -0,0 +1,37 @@
+// Primitive : 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreval = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 2623758946574635989879n
+
+
+
+
+// Non-Primitive (Reference)
+
+// 3 types : Array, Objects, Functions(Methods)
+
+
+const heroes = ["jack", 'rocky', "doga"];
+let myObj = {
+    name:"Ganesh",
+    age:21,
+}
+
+const myFunction =  function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);

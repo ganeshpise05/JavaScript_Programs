3cfe36281ed823ddf6b097e772f87edd2c8b316e
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-04 23:24:18 +0530

    String declaration

---
+++ 05_Strings.js
@@ -0,0 +1,19 @@
+
+const name = "Ganesh"
+const repoCount = 15
+
+//console.log(name + repoCount + " Value");
+
+// Use backticks``  for variable inject use ${}
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Funtya')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));

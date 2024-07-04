d03c3be4edc4fb202d3badd27c5e21280067ccce
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-04 23:26:52 +0530

    Inbuilt method used: length

--- 05_Strings.js
+++ 05_Strings.js
@@ -14,6 +14,6 @@ console.log(gameName[0]);
 console.log(gameName.__proto__);
 
 console.log(gameName);
-// console.log(gameName.length);
+console.log(gameName.length);
 // console.log(gameName.toUpperCase);
 // console.log(gameName.charAt(2));

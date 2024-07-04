a63f2b079997ff5fd46ecb266dbbf41ddf83e3c7
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-04 23:25:12 +0530

    which index which element present

--- 05_Strings.js
+++ 05_Strings.js
@@ -10,7 +10,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('Funtya')
 
-// console.log(gameName[0]);
+console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 // console.log(gameName);

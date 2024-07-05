e09a349450d59ab6e20bedb329529365eefd59ef
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-05 23:54:33 +0530

    primitive and non-primitive  datatypes

--- 05_Datatypes.js
+++ 05_Datatypes.js
@@ -1,20 +1,20 @@
-// // Primitive : 
+// Primitive : 
 
-// // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-// const score = 100
-// const scoreval = 100.3
+const score = 100
+const scoreval = 100.3
 
-// const isLoggedIn = false
-// const outsideTemp = null
-// let userEmail;
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
 
-// const id = Symbol('123')
-// const anotherId = Symbol('123')
+const id = Symbol('123')
+const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+console.log(id === anotherId);
 
-// const bigNumber = 2623758946574635989879n
+const bigNumber = 2623758946574635989879n
 
 
 

dfceea6ff500fc8cee2cbef0c4a9ee0f3b4f5a93
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-08 23:47:33 +0530

    else-if statement: Eg1

--- Statements.js
+++ Statements.js
@@ -6,3 +6,14 @@ let km = 2000
 if (km<=2000) {
     console.log("Do servicing of bike.");
 }
+
+// if-else statement
+
+//Example No:2
+
+let age = 12
+if (age>18) {
+    console.log("You are eligible for voting.");
+} else {
+    console.log("You are not eligible for voting.");
+}

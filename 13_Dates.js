be75d4f778b876888fce28a5a5a7f06c66617555
Ganesshsp05 <piseganesh2205@gmail.com>
2024-07-10 23:38:28 +0530

    date,time,year function called

--- 13_Dates.js
+++ 13_Dates.js
@@ -14,3 +14,30 @@ console.log(myDate.toLocaleTimeString()); // 11:19:55 pm
 
 console.log('----------------------------');
 console.log(typeof myDate);
+
+console.log('----------------------------');
+
+// Creating constant date
+
+let myCreatedDate = new Date(2024,5,10) // In this format jan start from 0 it's in string
+console.log(myCreatedDate.toLocaleDateString());
+
+let myCreatedDate2 = new Date("2024-07-10") // In "YYYY-MM-DD" Date start from 1
+console.log(myCreatedDate2.toLocaleString());
+
+let myCreatedDate3 = new Date(2024,5,10) //in "MM-DD-YYYY" 
+console.log(myCreatedDate3.toLocaleDateString());
+
+console.log('----------------------------');
+
+let myTimeStamp = Date.now() // Current date print
+console.log(myTimeStamp); // In mili-second
+console.log(Math.floor(Date.now()/1000)); // In Second
+
+console.log('----------------------------');
+
+let newDate = new Date()
+console.log(newDate); // Give today date
+console.log(newDate.getFullYear()); // year
+console.log(newDate.getMonth()); // Month ( January start from 0)
+console.log(newDate.getDay()); // Day

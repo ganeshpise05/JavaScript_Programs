b874cdd491d4b91fdcb9ad1061c7b23c30a1edb8
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:48:53 +0530

    Comparison  Operator

---
+++ ComparisonOperator.js
@@ -0,0 +1,19 @@
+//comparison operators :ans must be in true & false
+
+let comp1 = 6;
+let comp2 = 5;
+
+console.log("comp1==comp2", comp1 == comp2)
+
+console.log("comp1!=comp2", comp1 != comp2)
+
+//whe one is number & another is string then it shows false i.e a=6; b="5"
+console.log("comp1===comp2", comp1 === comp2)
+
+console.log("comp1!==comp2", comp1 !== comp2)
+
+console.log("comp1>comp2", comp1 > comp2)
+
+console.log("comp1>=comp2", comp1 >= comp2)
+
+console.log("comp1<=comp2", comp1 <= comp2)

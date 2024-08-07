67be87193d4c41acfb3c2a918977a29879e065c8
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:47:22 +0530

    Logical operators

---
+++ LogicalOperator.js
@@ -0,0 +1,13 @@
+//logocal operators in js
+
+let s = 5;
+let f = 4;
+console.log(s < f && f == 4)
+
+console.log(s < f || f == 4)
+
+console.log(!false)
+
+//ternary operator
+let salary = 8;
+console.log("You can marry saniya", (salary < 9 ? "yes" : "No"))

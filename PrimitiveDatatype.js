553ee793f462f3b2e99b48fe6645640dee741b09
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:45:14 +0530

    Primitive Datatype in js

---
+++ PrimitiveDatatype.js
@@ -0,0 +1,22 @@
+//primitive datatype nn ss bb u (null,number,symbol,string,boolean ,BigInt,undefined)
+
+/*let a=null;
+let b=33;
+let c=Symbol(" I am a Symbol")
+let d="Funtya"
+let e= true //can also false
+let f=BigInt("511")
+let g
+
+console.log(a,b,c,d,e,f,g, )
+console.log(typeof c) */
+
+//nonprimitive Datatype or Objects in Js 
+const item ={
+    "Funtya": true,
+    "Sanju" :false,
+    "Dipti":420,
+    "Tanmay":undefined
+    }
+    
+    console.log(item["Dipti"])   

74c603f3542b9b5378f79c1f56a6fc1a7ae2ca72
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:43:27 +0530

    var in js

---
+++ variables.js
@@ -0,0 +1,14 @@
+console.log("Variable in Java Script") 
+
+var a = 33 //a contains 33
+console.log(a)
+
+a = "Funtya"//string
+console.log(a)
+
+//var 5Saniya = 33 not allowed must begin with $,_,letter
+
+//reserved words can't be used as a variable name
+//var var = 13
+
+//it's the case sensitive i.e Saniya & saNiya are different variable

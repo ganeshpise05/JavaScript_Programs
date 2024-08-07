3e5e08ad02f03161ade3fbec6bbebe017030da8f
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:48:00 +0530

    Assignment Operator

---
+++ AssignmentOperator.js
@@ -0,0 +1,20 @@
+//These are the assignment operators
+
+let assignment = 4;
+assignment += 5 //assignment=assignment+5
+console.log(assignment)
+
+assignment -= 5 //assignment=assignment-5
+console.log(assignment)
+
+assignment *= 5 //assignment=assignment*5
+console.log(assignment)
+
+assignment /= 5 //assignment=assignment/5
+console.log(assignment)
+
+assignment %= 5 //assignment=assignment%5
+console.log(assignment)
+
+assignment **= 5 //assignment=assignment 
+console.log(assignment)

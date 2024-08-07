247fc3469025e8d6d82e13eacb81a31fa7a547d9
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:45:55 +0530

    Practice question

---
+++ Practice1.js
@@ -0,0 +1,34 @@
+//create a variable of type string and try to add a number to it. chapter1:Q1
+
+let a="Funtya"
+let b=33
+console.log(a+b)
+
+//chapter1:Q2
+console.log(typeof (a+b))
+
+//chapter1:Q3 
+const a1={
+  name:"Funtya",
+  section:1,
+  principal:false
+}
+  console.log(a1)
+  console.log(typeof (a1))
+
+//chapter1:Q4  
+a1['friend']="Raj"
+a1['name']='Abhi'
+console.log(a1)
+console.log(a1['name'])
+
+//chapter1:Q5
+const dict={
+  OatMilk:"Milk prepared from oats",
+  Onset:"The early stages",
+  Tract:"An area of land that is extended",
+  Abstruse: "Difficult to understand",
+  Calumny: "A false accusation of an offense" 
+}
+console.log(dict['Onset'])
+console.log(dict.Calumny)

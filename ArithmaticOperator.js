2973fd53546601f99c329002fabf47c062348889
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-07 23:49:47 +0530

    Arithmatic  Operator

---
+++ ArithmaticOperator.js
@@ -0,0 +1,23 @@
+//Arihtmatic operators in Js : +,-,*,/,%,++,--,**
+
+console.log("Arithmatic operators")
+let a = 45;
+let b = 4;
+
+console.log("a+b=", a + b)
+console.log("a-b=", a - b)
+console.log("a*b=", a * b)
+console.log("a/b=", a / b)
+console.log("a%b=", a % b)
+
+console.log("a++=", a++)
+console.log("a=", a)
+
+console.log("a--=", a--)
+console.log("a=", a)
+
+console.log("++a=", ++a)
+console.log("a=", a)
+
+console.log("--a=", --a)
+console.log("a=", a)

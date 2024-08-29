aaaa964c6118323746c01f35fd32a1c48cd1ab39
Ganesshsp05 <piseganesh2205@gmail.com>
2024-08-29 19:54:26 +0530

    .

--- 13_Dates.js
+++ 13_Dates.js
@@ -28,6 +28,7 @@ console.log(myCreatedDate2.toLocaleString());
 let myCreatedDate3 = new Date(2024,5,10) //in "MM-DD-YYYY" 
 console.log(myCreatedDate3.toLocaleDateString());
 
+
 console.log('----------------------------');
 
 let myTimeStamp = Date.now() // Current date print

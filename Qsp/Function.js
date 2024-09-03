ec7e69f340fbba477f8a6e80619cf87edeb0a99e
Ganesshsp05 <piseganesh2205@gmail.com>
2024-09-03 13:09:41 +0530

    parameterized function and Function without declaration

--- Qsp/Function.js
+++ Qsp/Function.js
@@ -12,25 +12,35 @@ firstFunct()
 let add = function(a,b){
     console.log(`Addition of ${a} and ${b} is : ${a+b}`);
 }
-add(100,200)
 
 let sub = function(c,d){
     console.log(`Substraction of ${c} and ${d} is : ${c-d}`);
 }
-sub(300,200)
 
 let multi = function(e,f){
     console.log(`Multiplication of ${e} and ${f} is : ${e*f}`); 
 }
-multi(5,10)
 
 let div = function(g,h){
     console.log(` Division of ${g} and ${h} is : ${g/h}`); 
 }
-div(5,10)
 
 let mod = function(i,j){
     console.log(` Modulus of ${i} and ${j} is : ${i%j}`); 
 }
+
+add(100,200) 
+sub(300,200)
+multi(5,10)
+div(5,10)
 mod(5,10)
+
+// Without declaring Funnction
+
+function WithoutDecla (a,b){
+    console.log(a,b,"function executed !");
+}
+WithoutDecla(10,20)
+
+
 

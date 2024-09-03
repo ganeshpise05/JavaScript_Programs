c617924526d3ea6d0e5948b208bf076a31a34e08
Ganesshsp05 <piseganesh2205@gmail.com>
2024-09-03 12:56:03 +0530

    parameterized function

--- Qsp/Function.js
+++ Qsp/Function.js
@@ -6,3 +6,31 @@ let firstFunct = function() {
 }
 
 firstFunct()
+
+// Parameterized Function 
+
+let add = function(a,b){
+    console.log(`Addition of ${a} and ${b} is : ${a+b}`);
+}
+add(100,200)
+
+let sub = function(c,d){
+    console.log(`Substraction of ${c} and ${d} is : ${c-d}`);
+}
+sub(300,200)
+
+let multi = function(e,f){
+    console.log(`Multiplication of ${e} and ${f} is : ${e*f}`); 
+}
+multi(5,10)
+
+let div = function(g,h){
+    console.log(` Division of ${g} and ${h} is : ${g/h}`); 
+}
+div(5,10)
+
+let mod = function(i,j){
+    console.log(` Modulus of ${i} and ${j} is : ${i%j}`); 
+}
+mod(5,10)
+

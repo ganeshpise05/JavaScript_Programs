74d4eafd49e302b04f3a08c252e046aaa78b806f
Ganesshsp05 <piseganesh2205@gmail.com>
2024-09-04 20:09:04 +0530

    function.js added

--- Qsp/Function.js
+++ Qsp/Function.js
@@ -104,3 +104,68 @@ Multiplication(5,10)
 Division(5,10)
 Modulus(5,10)
 console.log('-----------------------');
+
+// Introduction Using : Function Declaration , Parameterized Fuction , Function Without Declaration , Arrow Fuction
+
+ let Intro1 = (id,name,phno,sal) => {
+   console.log(`The employee Id: ${id} , Name: ${name} , Phone number: ${phno} , Salary: ${sal}`); 
+}
+
+Intro1(1,'Ganesh',8329988688,24500)
+
+let funct = () => {
+    console.log("Function executed !!"); 
+}
+
+funct();
+
+// Invoke  Function : Calling Itself
+
+((a) => { 
+    console.log("Function calling itself !",a)
+})
+(10);
+
+// ( function fun (a,b){
+//     console.log(`The value of a is: ${a} and b is: ${b}`); 
+// })
+// fun();
+
+// USer Input 
+
+// (() => { 
+//     let val = prompt("Enter your age: ")
+//     console.log(val); 
+// })
+// ();
+
+console.log("---------------------------------");
+
+// Higher Order Function and Call Back Function 
+
+// Example 1
+let hof = (cbf) => {
+    console.log("HOF executed !"); 
+    cbf()
+}
+
+let cbf = () => {
+    console.log("CBF executed !"); 
+}
+
+hof(cbf)
+
+console.log("--------");
+
+// Example 2
+let a = 10
+let hof1 = (b,cbfParameter) => {
+    cbfParameter()
+}
+ 
+let cbf1 = () => {
+    console.log("CBF1 executed !"); 
+}
+
+hof1(a,cbf1)
+
---
+++ Qsp/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let funct = () => {
+//     console.log("Function executed !!"); 
+// }
+
+// funct()

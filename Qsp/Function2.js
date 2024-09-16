f583a963da1ca4f130b78fe0dadbc646c9226344
Ganesshsp05 <piseganesh2205@gmail.com>
2024-09-16 13:07:24 +0530

    default parameter function

---
+++ Qsp/Function2.js
@@ -0,0 +1,23 @@
+// 16-09-2024
+
+// Addition
+let add = (a,b) => {
+    return a+b;
+};
+console.log(add(100)); //NaN
+
+
+let add2 = (a,b=0) => {
+    return a+b;
+};
+console.log(add2(100)); // b=0 default parameter
+
+// Find out the vowels in given string
+let str = "hello hunny bunny"
+let findVowels = (val) =>{
+    for(let i=0;i<=val.length-1;i++){
+        if (str[i]) {
+            
+        }
+    }
+}

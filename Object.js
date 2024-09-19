79d9836c6d93cde4a4aa75b77afbe96869f863d4
Ganesshsp05 <piseganesh2205@gmail.com>
2024-09-19 13:09:13 +0530

    create object by using literal

---
+++ Object.js
@@ -0,0 +1,15 @@
+console.log("creating an Object.");
+
+// Below object is created by using the object literal/keyword.
+
+let obj = {
+    name : "Ganesh",
+    age : 21,
+    mobileNo : 8329988688,
+    email : "piseganesh2205@gmail.com"
+}
+
+console.log(obj.name); // Ganesh
+console.log(obj); // Whole object print
+
+

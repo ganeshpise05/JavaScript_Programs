30269467a9b6c8f922f5248139c0423c3f867ec2
Ganesshsp05 <piseganesh2205@gmail.com>
2024-10-03 12:44:43 +0530

    we can't do addition of array element by using map()

--- Qsp/a.js
+++ Qsp/a.js
@@ -4,4 +4,10 @@ let sum = 0
 arr.forEach ((val,i) => {
     sum+=val
 })
-console.log(sum);
+console.log(sum); //150
+
+//Using map() method
+let addition = arr.map((val,i) => {
+    return val+=val
+})
+console.log(addition); // [ 20, 40, 60, 80, 100 ]

25361028039aa8821872c06d86bec5c2afc03ae7
Ganesshsp05 <piseganesh2205@gmail.com>
2024-10-14 23:25:42 +0530

    .

--- Qsp/Destructor.js
+++ Qsp/Destructor.js
@@ -93,6 +93,7 @@ conbineArray (...arr1 , ...arr2 , ...arr3);
 
 console.log("----------------");
 
+
 /*
 19.	**Array Destructuring with Spread**:  
     Given the array `const nums = [1, 2, 3, 4, 5]`, 

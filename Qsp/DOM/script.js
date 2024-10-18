c72dafe12aa2a3fac286a5bcf4d81faaa6c43cb0
Ganesshsp05 <piseganesh2205@gmail.com>
2024-10-18 13:42:37 +0530

    targeting html element by using QuerySelector

--- Qsp/DOM/script.js
+++ Qsp/DOM/script.js
@@ -14,7 +14,18 @@ for (let i = 0; i < input.length; i++) {
 
 */
 
+/*
 // 2:  Targeting element by using class name
 let userInput = document.getElementsByClassName("userInput")
 console.log(userInput);
+
+*/
+
+// 3:  Targeting element by using QuerySelector (only perticular tag/element target) ,QuerySelectorAll (for All tag target)
+let Input = document.querySelector(".userInput");
+console.log(Input);
+
+let Input1 = document.querySelectorAll(".userInput");
+console.log(Input1);
+
 
